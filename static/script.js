73234d4ac8a2af594730fc9a7df44f075018cd78
Carlos Navas <carlosnavas1991@gmail.com>
2016-02-17 15:08:02 +0100

    Bugfixes

--- static/script.js
+++ static/script.js
@@ -63,7 +63,7 @@ var startApp = function( list ){
 
           if( index === params.list.length - 1 ){
             loadPlaylist();
-            loadItem( playlist[indexPlaying] );
+            loadItem( indexPlaying );
           }
 
         }
@@ -83,14 +83,14 @@ var loadPlaylist = function(){
   playlist.forEach( function(item, index){
 
     var songItem = songPrototype.clone().removeClass('wz-prototype');
-		songItem.addClass('song-' + playlist[index].id);
+		songItem.addClass('song-id-' + playlist[index].id);
 		songItem.find('.title')
       .text( ( playlist[index].metadata && playlist[index].metadata.id3 && playlist[index].metadata.id3.title )? playlist[index].metadata.id3.title : playlist[index].name );
     songItem.find('.artist')
       .text( ( playlist[index].metadata && playlist[index].metadata.id3 && playlist[index].metadata.id3.artist && playlist[index].metadata.id3.artist[0] )? playlist[index].metadata.id3.artist[0] : lang.unknown );
     songItem.children('figure')
       .css( 'background-image', 'url(' + ( playlist[index].thumbnails['64'] ? playlist[index].thumbnails['64'] : 'https://static.inevio.com/app/5/cover.jpg' ) + ')' );
-    songItem.data( playlist[index] );
+    songItem.data( 'index' , index );
 
     var time  = playlist[index].metadata.media.duration.seconds;
     var hour  = parseInt(time/3600, 10);
@@ -111,20 +111,18 @@ var loadPlaylist = function(){
         songItem.find('.time').text(min+':'+sec);
     }
 
-    if( index === indexPlaying ){
-      songItem.addClass( 'active' );
-    }
-
 		toInsert.push( songItem );
 
   });
 
-  console.log( toInsert );
   playListDom.append( toInsert );
 
 }
 
-var loadItem = function( structure ){
+var loadItem = function( index ){
+
+  var structure = playlist[index];
+  console.log(structure);
 
   audio.empty();
   audio.append( $('<source></source>').attr('type','audio/mpeg').attr('src', structure.formats.mpeg.url) );
@@ -137,6 +135,13 @@ var loadItem = function( structure ){
 
   audio.load();
 
+  $('.song.active').removeClass('active');
+  $('.song-id-' + structure.id).addClass('active');
+
+  indexPlaying = index;
+  console.log(playlist);
+  console.log(structure);
+  console.log(indexPlaying);
   pointer++;
 
 };
@@ -155,6 +160,236 @@ win.on( 'app-param', function( e, params ){
   }*/
 
 });
+
+$( win )
+
+.on( 'click', '.song', function(){
+
+  loadItem( $(this).data('index') );
+
+})
+
+.on( 'wz-dragmove', '.music-volume-seeker', function( e, posX, posY ){
+
+    if( win.hasClass('muted') ){
+        audio[ 0 ].muted = false;
+    }
+
+    musicVolume.css( 'width', posX * musicMaxVolume.width() );
+
+    audio[ 0 ].volume = 1 * posX;
+
+})
+
+.on( 'wz-dragmove', '.music-time-seeker', function( e, posX, posY ){
+
+    audio[ 0 ].pause();
+
+    if( !emulatedSeekerTimer ){
+
+        emulatedSeekerTimer = setInterval( function(){
+            audio[ 0 ].currentTime = emulatedSeekerTime;
+        }, 100 );
+
+    }
+
+    musicProgress.css( 'width', posX * musicBackprogress.width() );
+
+    /*
+     * Como cambiar el currentTime de un elemento es un proceso costoso
+     * para el procesador, emulamos ese proceso
+     */
+    emulatedSeekerTime = audio[ 0 ].duration * posX;
+
+    var time      = audio[ 0 ].duration;
+    var totalHour = parseInt( time / 3600, 10 );
+    var rem       = time % 3600;
+    var totalMin  = parseInt( rem / 60, 10 );
+
+    time     = emulatedSeekerTime;
+    var hour = parseInt( time / 3600, 10 );
+
+    rem     = ( time % 3600 );
+    var min = parseInt( rem / 60, 10 );
+    var sec = parseInt( rem % 60, 10 );
+
+    if( totalHour > 9 && hour < 10 ){ hour = '0' + hour; }
+    if( totalHour > 0 || ( totalMin > 10 && min < 10 ) ){ min = '0' + min; }
+    if( sec < 10 ){ sec  = '0' + sec; }
+
+    if( totalHour ){
+        musicCurrentTime.text( hour + ':' + min + ':' + sec );
+    }else if( totalMin ){
+        musicCurrentTime.text( min + ':' + sec );
+    }else{
+        musicCurrentTime.text( '0:' + sec );
+    }
+
+})
+
+.on( 'mousedown', '.play-button.play', function(){
+
+    if( win.hasClass('playing') ){
+        audio[0].pause();
+    }else{
+        audio[0].play();
+    }
+
+})
+
+.on( 'mousedown', '.volume-icon', function(){
+
+    if( win.hasClass('muted') ){
+        audio[ 0 ].muted = false;
+    }else{
+        audio[ 0 ].muted = true;
+    }
+
+})
+
+.on( 'wz-dragend', '.music-time-seeker', function(){
+
+    clearInterval( emulatedSeekerTimer );
+
+    emulatedSeekerTimer    = 0;
+    audio[ 0 ].currentTime = emulatedSeekerTime;
+
+    audio[ 0 ].play();
+
+})
+
+.on( 'click', '.play-button.rewind', function(e){
+
+    //audio[0].currentTime -= 10;
+    console.log('rewind');
+    if( indexPlaying !== 0 ){
+      loadItem( indexPlaying - 1 )
+    }
+    e.stopPropagation();
+
+})
+
+.on( 'click', '.play-button.forward', function(e){
+
+    //audio[0].currentTime += 10;
+    console.log('forward');
+    if( indexPlaying !== playlist.length - 1 ){
+      loadItem( indexPlaying + 1 )
+    }
+    e.stopPropagation();
+
+})
+
+/*
+.on('mousedown', '.weemusic-info-random', function(){
+
+    if( randomize.hasClass('active') ){
+
+        randomize.removeClass('active');
+
+        for( var i = 0; i < pointers.length; i++ ){
+            pointers[ i ] = i;
+        }
+
+    }else{
+
+        randomize.addClass('active');
+
+        var a = pointers.slice(0, pointer);
+        var b = pointers.slice(pointer, pointers.length);
+        b.sort(randomly);
+        pointers = a.concat(b);
+
+    }
+
+})
+*/
+
+.on( 'mousedown', '.weemusic-controls-repeat', function(){
+    win.toggleClass('repeat');
+})
+
+.key('space', function(){
+
+    if( win.hasClass('playing') ){
+        audio[ 0 ].pause();
+    }else{
+        audio[ 0 ].play();
+    }
+
+})
+
+.key(
+
+    'right',
+    function(){ audio[ 0 ].currentTime += 10; }, // To Do -> Comprobar exceso
+    function(){ audio[ 0 ].currentTime += 10; }  // To Do -> Comprobar exceso
+
+)
+
+.key(
+
+    'left',
+    function(){ audio[ 0 ].currentTime -= 10; },
+    function(){ audio[ 0 ].currentTime -= 10; }
+
+)
+
+.key(
+
+    'up',
+    function(){
+
+        if( ( audio[ 0 ].volume + 0.1 ) < 1){
+            audio[ 0 ].volume += 0.1;
+        }else{
+            audio[ 0 ].volume = 1;
+        }
+
+    },
+    function(){
+
+        if( ( audio[ 0 ].volume + 0.1 ) < 1){
+            audio[ 0 ].volume += 0.1;
+        }else{
+            audio[ 0 ].volume = 1;
+        }
+
+    }
+
+)
+
+.key(
+
+    'down',
+    function(){
+
+        if( ( audio[ 0 ].volume - 0.1 ) > 0){
+            audio[ 0 ].volume -= 0.1;
+        }else{
+            audio[ 0 ].volume = 0;
+        }
+
+    },
+    function(){
+
+        if( ( audio[ 0 ].volume - 0.1 ) > 0){
+            audio[ 0 ].volume -= 0.1;
+        }else{
+            audio[ 0 ].volume = 0;
+        }
+
+    }
+
+)
+
+.key(
+
+    'backspace',
+    function(){ audio[ 0 ].currentTime = 0; },
+    function(){ audio[ 0 ].currentTime = 0; }
+
+);
 
 audio.on('durationchange', function(){
 
@@ -196,227 +431,7 @@ audio.on('durationchange', function(){
 
     audio[ 0 ].play();
 
-    $( win )
 
-    .on( 'click', '.song', function(){
-
-      loadItem( $(this).data() );
-      $('.song.active').removeClass('active');
-      $(this).addClass('active');
-
-    })
-
-    .on( 'wz-dragmove', '.music-volume-seeker', function( e, posX, posY ){
-
-        if( win.hasClass('muted') ){
-            audio[ 0 ].muted = false;
-        }
-
-        musicVolume.css( 'width', posX * musicMaxVolume.width() );
-
-        audio[ 0 ].volume = 1 * posX;
-
-    })
-
-    .on( 'wz-dragmove', '.music-time-seeker', function( e, posX, posY ){
-
-        audio[ 0 ].pause();
-
-        if( !emulatedSeekerTimer ){
-
-            emulatedSeekerTimer = setInterval( function(){
-                audio[ 0 ].currentTime = emulatedSeekerTime;
-            }, 100 );
-
-        }
-
-        musicProgress.css( 'width', posX * musicBackprogress.width() );
-
-        /*
-         * Como cambiar el currentTime de un elemento es un proceso costoso
-         * para el procesador, emulamos ese proceso
-         */
-        emulatedSeekerTime = audio[ 0 ].duration * posX;
-
-        var time      = audio[ 0 ].duration;
-        var totalHour = parseInt( time / 3600, 10 );
-        var rem       = time % 3600;
-        var totalMin  = parseInt( rem / 60, 10 );
-
-        time     = emulatedSeekerTime;
-        var hour = parseInt( time / 3600, 10 );
-
-        rem     = ( time % 3600 );
-        var min = parseInt( rem / 60, 10 );
-        var sec = parseInt( rem % 60, 10 );
-
-        if( totalHour > 9 && hour < 10 ){ hour = '0' + hour; }
-        if( totalHour > 0 || ( totalMin > 10 && min < 10 ) ){ min = '0' + min; }
-        if( sec < 10 ){ sec  = '0' + sec; }
-
-        if( totalHour ){
-            musicCurrentTime.text( hour + ':' + min + ':' + sec );
-        }else if( totalMin ){
-            musicCurrentTime.text( min + ':' + sec );
-        }else{
-            musicCurrentTime.text( '0:' + sec );
-        }
-
-    })
-
-    .on( 'mousedown', '.play-button.play', function(){
-
-        if( win.hasClass('playing') ){
-            audio[0].pause();
-        }else{
-            audio[0].play();
-        }
-
-    })
-
-    .on( 'mousedown', '.volume-icon', function(){
-
-        if( win.hasClass('muted') ){
-            audio[ 0 ].muted = false;
-        }else{
-            audio[ 0 ].muted = true;
-        }
-
-    })
-
-    .on( 'wz-dragend', '.music-time-seeker', function(){
-
-        clearInterval( emulatedSeekerTimer );
-
-        emulatedSeekerTimer    = 0;
-        audio[ 0 ].currentTime = emulatedSeekerTime;
-
-        audio[ 0 ].play();
-
-    })
-
-    .on( 'mousedown', '.play-button.rewind', function(){
-
-        audio[0].currentTime -= 10;
-
-    })
-
-    .on( 'mousedown', '.play-button.forward', function(){
-
-        audio[0].currentTime += 10;
-
-    })
-
-    /*
-    .on('mousedown', '.weemusic-info-random', function(){
-
-        if( randomize.hasClass('active') ){
-
-            randomize.removeClass('active');
-
-            for( var i = 0; i < pointers.length; i++ ){
-                pointers[ i ] = i;
-            }
-
-        }else{
-
-            randomize.addClass('active');
-
-            var a = pointers.slice(0, pointer);
-            var b = pointers.slice(pointer, pointers.length);
-            b.sort(randomly);
-            pointers = a.concat(b);
-
-        }
-
-    })
-    */
-
-    .on( 'mousedown', '.weemusic-controls-repeat', function(){
-        win.toggleClass('repeat');
-    })
-
-    .key('space', function(){
-
-        if( win.hasClass('playing') ){
-            audio[ 0 ].pause();
-        }else{
-            audio[ 0 ].play();
-        }
-
-    })
-
-    .key(
-
-        'right',
-        function(){ audio[ 0 ].currentTime += 10; }, // To Do -> Comprobar exceso
-        function(){ audio[ 0 ].currentTime += 10; }  // To Do -> Comprobar exceso
-
-    )
-
-    .key(
-
-        'left',
-        function(){ audio[ 0 ].currentTime -= 10; },
-        function(){ audio[ 0 ].currentTime -= 10; }
-
-    )
-
-    .key(
-
-        'up',
-        function(){
-
-            if( ( audio[ 0 ].volume + 0.1 ) < 1){
-                audio[ 0 ].volume += 0.1;
-            }else{
-                audio[ 0 ].volume = 1;
-            }
-
-        },
-        function(){
-
-            if( ( audio[ 0 ].volume + 0.1 ) < 1){
-                audio[ 0 ].volume += 0.1;
-            }else{
-                audio[ 0 ].volume = 1;
-            }
-
-        }
-
-    )
-
-    .key(
-
-        'down',
-        function(){
-
-            if( ( audio[ 0 ].volume - 0.1 ) > 0){
-                audio[ 0 ].volume -= 0.1;
-            }else{
-                audio[ 0 ].volume = 0;
-            }
-
-        },
-        function(){
-
-            if( ( audio[ 0 ].volume - 0.1 ) > 0){
-                audio[ 0 ].volume -= 0.1;
-            }else{
-                audio[ 0 ].volume = 0;
-            }
-
-        }
-
-    )
-
-    .key(
-
-        'backspace',
-        function(){ audio[ 0 ].currentTime = 0; },
-        function(){ audio[ 0 ].currentTime = 0; }
-
-    );
 
     audio
     .on('play',function(){
